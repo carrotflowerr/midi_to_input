d6eeb5aaec4b01846069306a8548992f5fa9b4de
carrotflowerr <64298096+carrotflowerr@users.noreply.github.com>
2023-06-20 04:33:09 +0000

    Update main.py
    debounce delay

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
 import rtmidi
 import os
 
-data_byte1 = 0  
+data_byte1 = 0  # 
+last_trigger_time = time.time()  # last trigger time
+
+DEBOUNCE_DELAY = 0.5  # Debounce delay
 
 def process_midi_message(message):
     global data_byte1
@@ -14,38 +18,45 @@ def process_midi_message(message):
             # MIDI note number is data_byte1
             print("Pad", data_byte1, "pressed")
             # Velocity/intensity is data_byte2
-            print("Velocity:", data_byte2)
+            #print("Velocity:", data_byte2)
 
 def macros(data_byte):
-    """  
-    this is where your code goes.
-    
-    ex. 
-    if data_byte == 112: os.system("gnome-terminal")
-    if data_byte == 113: os.system("firefox")
-    """
+    global last_trigger_time
+    current_time = time.time()
+    if current_time - last_trigger_time >= DEBOUNCE_DELAY:
+        """
+        this is where your commands should go.
+        ex.
+        if data_byte == 112:
+            os.system("gnome-terminal")
+        elif data_byte == 113:
+            os.system("firefox")
+        elif data_byte == 114:
+            os.system("steam")
+        """
+        last_trigger_time = current_time
 
 def main():
     midi_in = rtmidi.MidiIn()
 
-    # Print ports
+    # Print the available MIDI input ports
     print("Available MIDI input ports:")
     for port_number, port_name in enumerate(midi_in.get_ports()):
         print(f"[{port_number}] {port_name}")
 
-    # Choose MIDI input port
+    # Choose the MIDI input port to listen to
     selected_port_number = int(input("Enter the port number of the MIDI input: "))
 
-
+    # Open the selected MIDI input port
     midi_in.open_port(selected_port_number)
 
     print("Listening to MIDI input on", midi_in.get_port_name(selected_port_number))
 
-    # capture 
+    # Start an infinite loop to capture MIDI messages
     while True:
         message = midi_in.get_message()
         if message:
-            message, _ = message  # Extract from tuple
+            message, _ = message  # Extract the MIDI message from the tuple
             process_midi_message(message)
             macros(data_byte1)  # Pass data_byte1 to the function
 
